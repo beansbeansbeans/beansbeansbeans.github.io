71ae8ea7698bec1927aaa4de8bb88025bfeec1d6
Ann Yuan <ayuan@thisisgrow.com>
2014-07-02 09:08:51 -0400

    maps settled in scss

--- js/projects.js
+++ js/projects.js
@@ -7,6 +7,18 @@ define(['templates/project_index'], function(projectIndex) {
 		{
 			title: "hiveplot",
 			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non explicabo porro id perferendis, officia alias!"
+		},
+		{
+			title: "spotlights",
+			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, temporibus!"
+		},
+		{
+			title: "leaves",
+			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem fugit veritatis, aliquid?"
+		},
+		{
+			title: "shadows",
+			description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam at dicta a itaque."
 		}
 	];
 	var projects = {
