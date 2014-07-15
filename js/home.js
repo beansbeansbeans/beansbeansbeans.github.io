ce699b80f7f09c716bd7c397f8e2f91b51246785
Ann Yuan <ayuan@thisisgrow.com>
2014-07-15 06:36:26 -0400

    shadow rough draft

--- js/home.js
+++ js/home.js
@@ -1,11 +1,12 @@
 define([], function() {
 	var home = {
 		initialize: function() {
-			$("#view").html("<div id='homepage'><h1>home</h1><div id='top'></div></div>");
+			$("#view").html("<div id='homepage'><h1>home</h1><div id='top'></div><div id='shadow'></div></div>");
 
 			var counter = 0,
 				frameCount = 25,
 				frameWidth = 341,
+				shadowWidth = 402,
 				frameInterval = 4,
 				self = this;
 
@@ -14,6 +15,7 @@ define([], function() {
 				if(counter%frameInterval == 0) {
 					var frame = Math.floor(counter / frameInterval) % frameCount;
 					$("#top").css("background-position", (frame * frameWidth) + "px 0px");
+					$("#shadow").css("background-position", (frame * shadowWidth) + "px 0px");
 				}
 				counter++;
 				/*
