7f31c85c7e24a2678e66d70fe3efccb5d092eb98
Ann Yuan <ayuan@thisisgrow.com>
2014-10-01 21:01:00 -0400

    fan

--- js/projects/dancers.js
+++ js/projects/dancers.js
@@ -12,6 +12,10 @@ define(['lib/d3', 'templates/project_detail'], function(d3, projectTemplate) {
 				description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem animi esse eligendi iste omnis nisi quidem itaque doloremque distinctio beatae?"
 			};
 
+			var manipulating = false;
+
+			var boundingBoxes = [];
+
 			$("#view").html(projectTemplate(data));
 
 			var maxTension = 1,
@@ -22,23 +26,69 @@ define(['lib/d3', 'templates/project_detail'], function(d3, projectTemplate) {
 
 			var svg = d3.select("#project-dancers .project-contents")
 				.append("svg")
+					.attr("id", "dancerGroup")
 					.attr("width", $(window).width())
 					.attr("height", 500)
-				.append("g");
+				.append("g")
+					.on("mouseup", function() {
+						console.log("mouse up");
+						// this.intervalID = setInterval(animateDancers, 13500);
+						// manipulating = false;
+					});
+
+			$("#dancerGroup").on("click", function(e) {
+				var closest = null,
+					closestFrameIndex,
+					goal = e.offsetX;
+
+				boundingBoxes.forEach(function(box, index) {
+					var midPoint = box.x + box.width / 2;
+					if(closest == null || Math.abs(midPoint - goal) < Math.abs(closest - goal)) {
+						closest = midPoint;
+						closestFrameIndex = index;
+					}
+				});
+
+				clearInterval(this.intervalID);
+				d3.selectAll(".dancer").attr("class", "dancer");
+				manipulating = true;
+				fan(closestFrameIndex);
+			}.bind(this));
+
+			var fan = function(start) {
+				var opacity = 1,
+					index = 1,
+					left = start,
+					right = start;
+
+				while(opacity > 0.1) {
+					(function(o, i, l, r) {
+						setTimeout(function() {
+							$(".dancer:eq(" + l + ")").css("opacity", o);
+							$(".dancer:eq(" + r + ")").css("opacity", o);
+						}, 50 * i);
+					})(opacity, index, left, right);
+
+					opacity -= 0.1;
+					index++;
+					left--;
+					right++;
+				}
+			};
 
 			var morph = function(element, data, initialTension) {
-					var newTension = ((initialTension - 0.01) < minTension ? minTension : initialTension - 0.01);
+				var newTension = ((initialTension - 0.01) < minTension ? minTension : initialTension - 0.01);
 
-					element.attr("data-tension", newTension);
+				element.attr("data-tension", newTension);
 
-					element.selectAll("path")[0].forEach(function(d, i) {
-						d3.select(d).attr("d", line.tension(newTension)(data[i]));
-					});
+				element.selectAll("path")[0].forEach(function(d, i) {
+					d3.select(d).attr("d", line.tension(newTension)(data[i]));
+				});
 
-					this.rafID = requestAnimationFrame(function() {
-						morph(element, data, newTension);
-					});
-				}.bind(this);
+				this.rafID = requestAnimationFrame(function() {
+					morph(element, data, newTension);
+				});
+			}.bind(this);
 
 			var animateDancers = function() {
 				var delay = 0;
@@ -48,12 +98,16 @@ define(['lib/d3', 'templates/project_detail'], function(d3, projectTemplate) {
 
 					(function(time) {
 						setTimeout(function() {
-							d3.select(d).attr("class", "dancer on");
+							if(!manipulating) {
+								d3.select(d).attr("class", "dancer on");
+							}
 						}, time);
 					})(delay);
 					delay += 100;
 				});
 			}
+
+			window.boxes = boundingBoxes;
 
 			d3.json("/js/projects/dancers.json", function(data) {
 				var self = this;
@@ -64,6 +118,11 @@ define(['lib/d3', 'templates/project_detail'], function(d3, projectTemplate) {
 					dancer.forEach(function(path) {
 						dancerGroup.append("path")
 							.attr("d", line.tension(maxTension)(path));
+					});
+
+					boundingBoxes.push({
+						x: dancerGroup.node().getBBox().x,
+						width: dancerGroup.node().getBBox().width
 					});
 
 					dancerGroup.attr("data-tension", maxTension)
