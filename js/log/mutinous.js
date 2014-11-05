2bcbd321b8b1b9d238668beb62665c0605508bc9
Ann Yuan <ayuan@thisisgrow.com>
2014-11-05 18:34:19 -0500

    entry

---
+++ js/log/mutinous.js
@@ -0,0 +1,27 @@
+define(['templates/log_detail', 'log/log_renderer'], function(logTemplate, logRenderer) {
+    var mutinous = {
+        initialize: function() {
+            var data = {
+                title: "mutinous",
+                overrides: [
+                    {
+                        index: 2,
+                        width: 1225
+                    }
+                ],
+                length: 99,
+                detail: "An average day",
+                date: "November 5, 2014",
+                logContents: ""
+            };
+
+            $("#view").html(logTemplate(data));
+
+            logRenderer.initialize(data);
+        },
+        destroy: function() {
+            logRenderer.destroy();
+        }
+    }
+    return mutinous;
+});
