383f875940bc6137b88c5487973af5a1d7c45be7
Ann Yuan <ayuan@thisisgrow.com>
2014-12-01 11:09:03 -0500

    progress

--- js/projects/spinny.js
+++ js/projects/spinny.js
@@ -7,7 +7,7 @@ define(['lib/d3', 'templates/project_detail'], function(d3, projectTemplate) {
 				identifier: "spinny",
 				title: "Spinny",
 				blurb: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, voluptatibus.",
-				projectContents: '<div id="triptych"><div id="spinny_globe"></div><div id="phone_mask"></div></div>',
+				projectContents: '<div id="triptych"><div id="spinny_globe"></div><div id="phone_mask"></div><div id="globe_mask"></div></div>',
 				caption: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, fugit.",
 				description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, nobis inventore id sequi non quam mollitia natus eum assumenda placeat."
 			},
@@ -87,6 +87,7 @@ define(['lib/d3', 'templates/project_detail'], function(d3, projectTemplate) {
 			});
 
 			$("#phone_mask").css("background-size", ((paneWidth / nativePaneWidth) * 232) + "px auto");
+			$("#globe_mask").css("background-size", ((paneWidth / nativePaneWidth) * 613) + "px auto");
 
 			mainPane.css({
 				width: paneWidth,
