f73058e48a04ed76ceea17a1603d46dac3deb50c
Ann Yuan <ayuan@thisisgrow.com>
2014-12-12 13:01:54 -0500

    basics of fire

--- js/projects/fire.js
+++ js/projects/fire.js
@@ -5,15 +5,66 @@ define(['lib/d3', 'templates/project_detail'], function(d3, projectTemplate) {
 				identifier: "fire",
 				title: "Fire",
 				blurb: "A game.",
-				projectContents: '<div></div>',
+				projectContents: '<div id="frame"></div>',
 				caption: "Built with JavaScript",
 				description: "This is a game."
-			}
+			},
+			numColumns = 10,
+			numRows = 6,
+			widthFrame = 850,
+			widthOverHeight = 1.77,
+			numFrames = 2,
+			framesPerSecond = 10,
+			cells = [];
 
 			$("#view").html(projectTemplate(data));
-		},
-		destroy: function() {
 
+			var frame = $("#frame"),
+				cellWidth = (widthFrame / numColumns).toFixed(1),
+				cellHeight = (widthFrame / widthOverHeight / numRows).toFixed(1);
+
+			for(var i = 0; i < numRows; i++) {
+				var row = $("<div class='row'></div>");
+				row.appendTo(frame);
+				for(var j = 0; j < numColumns; j++) {
+					$("<div class='cell'></div>").appendTo(row).css({
+						width: cellWidth,
+						height: cellHeight,
+						backgroundPosition: (-j * cellWidth) + "px " + (-i * cellHeight) + "px"
+					});
+
+					cells.push({
+						frame: 0,
+						direction: 1
+					});
+				}
+			}
+
+			var rafCounter = 0,
+				cycle = function() {
+					if(rafCounter % (60 / framesPerSecond) == 0) {
+						cells.forEach(function(cell, cellIndex) {
+							var cellEl = frame.find(".row:eq(" + Math.floor(cellIndex / numColumns) + ")").find(".cell:eq(" + (cellIndex % numColumns) + ")");
+
+							cellEl.css("background-image", "url(../images/project_fire/" + cell.frame + ".jpg)");
+
+							cell.frame = (cell.frame + cell.direction) % numFrames;
+						});
+					}
+					rafCounter++;
+					this.rafID = requestAnimationFrame(cycle);
+				}.bind(this);
+
+			this.rafID = requestAnimationFrame(cycle);
+
+			window.stop = function() {
+				window.cancelAnimationFrame(this.rafID);
+			}.bind(this);
+
+		},
+		rafID: null,
+		destroy: function() {
+			window.cancelAnimationFrame(this.rafID);
 		}
 	}
 	return fire;
