21c52fe9b9e1bcc0f45ee6815a441a6a0ab2c68a
Ann Yuan <ayuan@thisisgrow.com>
2015-09-13 16:09:49 -0400

    update copy

--- js/templates/log_index.js
+++ js/templates/log_index.js
@@ -9,7 +9,7 @@ return Handlebars.template({"1":function(depth0,helpers,partials,data) {
     + escapeExpression(((helper = helpers.description || (depth0 && depth0.description)),(typeof helper === functionType ? helper.call(depth0, {"name":"description","hash":{},"data":data}) : helper)))
     + " &mdash;</div>\n    		</a>\n	    </li>\n    ";
 },"compiler":[5,">= 2.0.0"],"main":function(depth0,helpers,partials,data) {
-  var stack1, buffer = "<nav>\n  <div class=\"explanation\">This is a log of how I've wasted time on the internet.</div>\n	";
+  var stack1, buffer = "<nav>\n  <div class=\"explanation\">This is a log of my media consumption.</div>\n	";
   stack1 = helpers.each.call(depth0, depth0, {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   return buffer + "\n</nav>";
