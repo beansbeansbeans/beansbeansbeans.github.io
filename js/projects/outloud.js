0472c3abb8125416b9d0fc50c67540788cfdb87e
Ann Yuan <ayuan@thisisgrow.com>
2015-09-25 06:13:42 -0400

    stuff

--- js/projects/outloud.js
+++ js/projects/outloud.js
@@ -6,7 +6,7 @@ define(['templates/project_detail'], function(projectTemplate) {
         identifier: "outloud",
         title: "outloud",
         blurb: "",
-        projectContents: '<div id="screenshot"><div id="image"></div></div>',
+        projectContents: '<a href="https://outloud.cc" target="_blank" id="screenshot"><div id="image"></div><div class="cta">View project</div></a>',
         caption: "Screenshot from <a href='https://outloud.cc' target='_blank'>Outloud's</a> recording page.",
         description: "<p>Podcasting and voicemail are decades-old technologies, yet they are the technological frontier for sharing audio. At the same time, tools abound for sharing images and video on the internet.</p><p>I built <a href='https://outloud.cc' target='_blank'>Outloud</a> to enable people to create and share 20 second recordings. Recordings are organized by topic, e.g. 'Jokes', or 'In My Humble Opinion'. Users can record reactions to what they hear on the site, which are then appended to the seed post.</p>"
       };
