87722a5fa45738fa9ffea5eaa1398f220d8e8282
Ann Yuan <ayuan@thisisgrow.com>
2015-09-25 07:37:14 -0400

    oughtness shell

--- js/projects/oughtness.js
+++ js/projects/oughtness.js
@@ -6,9 +6,9 @@ define(['templates/project_detail'], function(projectTemplate) {
         identifier: "oughtness",
         title: "oughtness",
         blurb: "",
-        projectContents: '<a href="https://storiesof.cc" target="_blank" id="screenshot"><div id="image"></div><div class="cta">View project</div></a>',
-        caption: "Screenshot from a user's moods on <a href='https://storiesof.cc' target='_blank'>Stories of</a>.",
-        description: "<a class='cta-main' href='https://storiesof.cc' target='_blank'>View project</a><p>Social media has become a tool for putting our best face forward. We use Facebook to share happy moments and successes, keeping the bad news to ourselves. Someone feeling low may get the impression from their social media feeds that they're alone in their experience.</p><p>I built <a href='https://storiesof.cc' target='_blank'>Stories of</a> as a way for people to share their moods, good or bad.</p>"
+        projectContents: '<a href="https://oughtness.cc" target="_blank" id="screenshot"><div id="image"></div><div class="cta">View project</div></a>',
+        caption: "Simplified version of <a href='https://oughtness.cc' target='_blank'>Oughtness's</a> voting page.",
+        description: "<a class='cta-main' href='https://oughtness.cc' target='_blank'>View project</a><p>Social media has become a tool for putting our best face forward. We use Facebook to share happy moments and successes, keeping the bad news to ourselves. Someone feeling low may get the impression from their social media feeds that they're alone in their experience.</p><p>I built <a href='https://oughtness.cc' target='_blank'>Stories of</a> as a way for people to share their moods, good or bad.</p>"
       };
 
       $("#view").html(projectTemplate(data));
