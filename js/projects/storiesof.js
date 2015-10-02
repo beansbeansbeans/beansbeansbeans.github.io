c5195a1bdda431262ce1dae865517e2f58a7c3e2
Ann Yuan <ayuan@thisisgrow.com>
2015-10-02 06:21:54 -0400

    new vid

--- js/projects/storiesof.js
+++ js/projects/storiesof.js
@@ -8,14 +8,14 @@ define(['templates/project_detail'], function(projectTemplate) {
         title: "storiesof",
         blurb: "Mood tracking.",
         projectContents: '<div class="chrome"><div class="chrome-controls"><div class="close"></div><div class="min"></div><div class="max"></div></div><div class="contents"><video src="/video/storiesof.mp4"></video></div></div>',
-        caption: "<div class='loading'>Loading video...</div><div class='caption-0'>Browse latest stories.</div><div class='caption-1'>Draw a mood path to find matching stories.</div><div class='caption-2'>Browse through the story.</div><div class='caption-3'>Show appreciation for the story.</div>",
+        caption: "<div class='loading'>Loading video...</div><div class='caption-0'>Browse latest stories.</div><div class='caption-1'>Find stories matching a mood path.</div><div class='caption-2'>Browse a matching story.</div><div class='caption-3'>Show appreciation for the story.</div>",
         description: "<a class='cta-main' href='https://storiesof.cc' target='_blank'>View project</a><p>Social media has become a tool for projecting the best and happiest versions of ourselves to the world. It's gotten to a point where if one is feeling low, the endless narrative of people enjoying themselves that makes up his social media feeds can almost seem mocking.</p><p>I built Stories of as a concept for a social network where users rate their moods over time to build 'stories'. Text is optional - the stories' emphasis is on the user's raw mood ratings and their trajectory. Users can search for stories that follow a certain path by drawing it on the search page.</p><p>I thought that a collection of mood histories showing how people's outlooks peak and valley and peak again could be valuable to people feeling unhappy. Such people often have trouble imagining themselves feeling back to normal again, and I wondered whether seeing evidence of other people bouncing back could help them escape from that destructive line of thought. </p>"
       };
 
       $("#view").html(projectTemplate(data));
 
       var video = $("video").get(0);
-      var script = [ 0, 3000, 10000, 13000 ];
+      var script = [ 0, 3500, 12000, 15000 ];
       var scriptIndex = 0;
 
       var loop = function() {
