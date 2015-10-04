023e334d0bc12c404d3b2f26d5cf6b401615f898
Ann Yuan <ayuan@thisisgrow.com>
2015-10-04 11:02:51 -0400

    new video for out loud

--- js/projects/outloud.js
+++ js/projects/outloud.js
@@ -1,22 +1,39 @@
 define(['templates/project_detail'], function(projectTemplate) {
   var outloud = {
     intervalID: null,
+    scriptTimeouts: [],
     initialize: function() {
       var data = {
         identifier: "outloud",
-        title: "outloud",
-        blurb: "",
-        projectContents: '<a href="https://outloud.cc" target="_blank" id="screenshot"><div id="image"></div><div class="cta">View project</div></a>',
-        caption: "Screenshot from <a href='https://outloud.cc' target='_blank'>Outloud's</a> recording page.",
+        title: "<a href='https://outloud.cc' target='_blank'>OUTLOUD</a>",
+        blurb: "Twitter for voice",
+        projectContents: '<div class="chrome"><div class="chrome-controls"><div class="close"></div><div class="min"></div><div class="max"></div></div><div class="contents"><video src="/video/outloud.mp4"></video></div></div>',
+        caption: "<div class='loading'>Loading video...</div><div class='caption-0'>Browse latest stories.</div><div class='caption-1'>Find stories matching a mood path.</div><div class='caption-2'>Browse a matching story.</div><div class='caption-3'>Show appreciation for the story.</div>",
         description: "<a class='cta-main' href='https://outloud.cc' target='_blank'>View project</a><p>Podcasting and voicemail are decades-old technologies, yet they are the technological frontier for sharing audio. At the same time, tools abound for sharing images and video on the internet.</p><p>I built <a href='https://outloud.cc' target='_blank'>Outloud</a> to enable people to create and share 20 second audio recordings. Recordings are organized by topic, e.g. 'Jokes', or 'In My Humble Opinion'. Users can record reactions to what they hear on the site, which are appended to the seed post.</p>"
       };
 
       $("#view").html(projectTemplate(data));
 
+      var video = $("video").get(0);
+      var script = [ 0, 3500, 12000, 15000 ];
+      var scriptIndex = 0;
+
+      var loop = function() {
+        video.play();
+        script.forEach(function(d, i) {
+          this.scriptTimeouts.push(setTimeout(function() {
+            $(".caption").attr("data-active-caption", i);
+          }, d));
+        }.bind(this));
+      }.bind(this);
+
+      video.addEventListener("canplaythrough", loop);
+      video.addEventListener("ended", loop);
       
       },
       destroy: function() {
         window.clearInterval(this.intervalID);
+        this.scriptTimeouts.forEach(window.clearTimeout);
       }
     };
     return outloud;
