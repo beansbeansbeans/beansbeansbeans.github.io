5d1e50d1275e8c1e2034d37da1fb6f634e89d4b0
Ann Yuan <ayuan@thisisgrow.com>
2015-10-31 17:19:00 -0400

    next and previous

--- js/projects.js
+++ js/projects.js
@@ -1,54 +1,5 @@
-define(['templates/project_index'], function(projectIndex) {
-	var data = [
-		{
-			title: "oughtness",
-			description: "Prioritizing problems."
-		},
-		{
-			title: "outloud",
-			description: "Twitter for audio."
-		},
-		{
-			title: "storiesof",
-			description: "Sharing unhappiness."
-		},
-		{
-			title: "pixels",
-			description: "A face made of warpable pixels."
-		},
-		{
-			title: "notes",
-			description: "A random melody maker."
-		},
-		{
-			title: "dancers",
-			description: "A ballet dancer with morphing frames."
-		},
-		{
-			title: "spinny",
-			description: "An interactive photograph."
-		},
-		{
-			title: "food",
-			description: "Words made of simplifiable paths."
-		},
-		{
-			title: "spotlights",
-			description: "A light-sensitive bouncing ball."
-		},
-		{
-			title: "straws",
-			description: "Straws resting inside an unstable glass."
-		},
-		{
-			title: "leaves",
-			description: "Leaves floating on top of sliding panes."
-		},
-		{
-			title: "hiveplot",
-			description: "A dependency graph visualization."
-		}
-	];
+define(['templates/project_index', 'project_data'], function(projectIndex, projectData) {
+	var data = projectData;
 	var projects = {
 		needsLoading: true,
 		preloadAssets: [ ],
--- js/templates/project_detail.js
+++ js/templates/project_detail.js
@@ -2,7 +2,15 @@ define(['handlebars'], function(Handlebars) {
 return Handlebars.template({"compiler":[5,">= 2.0.0"],"main":function(depth0,helpers,partials,data) {
   var stack1, helper, functionType="function", escapeExpression=this.escapeExpression, buffer = "<div class=\"project\" id=\"project-"
     + escapeExpression(((helper = helpers.identifier || (depth0 && depth0.identifier)),(typeof helper === functionType ? helper.call(depth0, {"name":"identifier","hash":{},"data":data}) : helper)))
-    + "\">\n	\n	<a href=\"#projects\" class=\"back\">back</a>\n	<div class=\"header\">\n		<div class=\"title\">";
+    + "\">\n	<a href=\"#projects/"
+    + escapeExpression(((helper = helpers.next || (depth0 && depth0.next)),(typeof helper === functionType ? helper.call(depth0, {"name":"next","hash":{},"data":data}) : helper)))
+    + "\" id=\"next\" data-active='"
+    + escapeExpression(((helper = helpers.nextActive || (depth0 && depth0.nextActive)),(typeof helper === functionType ? helper.call(depth0, {"name":"nextActive","hash":{},"data":data}) : helper)))
+    + "'>next</a>\n	<a href=\"#projects/"
+    + escapeExpression(((helper = helpers.previous || (depth0 && depth0.previous)),(typeof helper === functionType ? helper.call(depth0, {"name":"previous","hash":{},"data":data}) : helper)))
+    + "\" id=\"previous\" data-active='"
+    + escapeExpression(((helper = helpers.previousActive || (depth0 && depth0.previousActive)),(typeof helper === functionType ? helper.call(depth0, {"name":"previousActive","hash":{},"data":data}) : helper)))
+    + "'>previous</a>\n	<a href=\"#projects\" class=\"back\">back</a>\n	<div class=\"header\">\n		<div class=\"title\">";
   stack1 = ((helper = helpers.title || (depth0 && depth0.title)),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper));
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "</div>\n		<div class=\"blurb\">"
