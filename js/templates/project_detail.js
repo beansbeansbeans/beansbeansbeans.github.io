447a9ca48b33ecde182cfb81e9b4da5a7a306ed2
Ann Yuan <ayuan@thisisgrow.com>
2015-11-08 11:14:07 -0500

    improve prev/next styling

--- js/templates/project_detail.js
+++ js/templates/project_detail.js
@@ -6,11 +6,15 @@ return Handlebars.template({"compiler":[5,">= 2.0.0"],"main":function(depth0,hel
     + escapeExpression(((helper = helpers.next || (depth0 && depth0.next)),(typeof helper === functionType ? helper.call(depth0, {"name":"next","hash":{},"data":data}) : helper)))
     + "\" id=\"next\" data-active='"
     + escapeExpression(((helper = helpers.nextActive || (depth0 && depth0.nextActive)),(typeof helper === functionType ? helper.call(depth0, {"name":"nextActive","hash":{},"data":data}) : helper)))
-    + "'>next</a>\n	<a href=\"#projects/"
+    + "'>\n		<div class=\"project-preview\">"
+    + escapeExpression(((helper = helpers.next || (depth0 && depth0.next)),(typeof helper === functionType ? helper.call(depth0, {"name":"next","hash":{},"data":data}) : helper)))
+    + "</div>\n		<svg><path d=\"M10.129,22.186 16.316,15.999 10.129,9.812 13.665,6.276 23.389,15.999 13.665,25.725z\"></path></svg>\n	</a>\n	<a href=\"#projects/"
     + escapeExpression(((helper = helpers.previous || (depth0 && depth0.previous)),(typeof helper === functionType ? helper.call(depth0, {"name":"previous","hash":{},"data":data}) : helper)))
     + "\" id=\"previous\" data-active='"
     + escapeExpression(((helper = helpers.previousActive || (depth0 && depth0.previousActive)),(typeof helper === functionType ? helper.call(depth0, {"name":"previousActive","hash":{},"data":data}) : helper)))
-    + "'>previous</a>\n	<a href=\"#projects\" class=\"back\">back</a>\n	<div class=\"header\">\n		<div class=\"title\">";
+    + "'>\n		<div class=\"project-preview\">"
+    + escapeExpression(((helper = helpers.previous || (depth0 && depth0.previous)),(typeof helper === functionType ? helper.call(depth0, {"name":"previous","hash":{},"data":data}) : helper)))
+    + "</div>\n		<svg><path d=\"M21.871,9.814 15.684,16.001 21.871,22.188 18.335,25.725 8.612,16.001 18.335,6.276z\"></path></svg>\n	</a>\n	<a href=\"#projects\" class=\"back\">back</a>\n	<div class=\"header\">\n		<div class=\"title\">";
   stack1 = ((helper = helpers.title || (depth0 && depth0.title)),(typeof helper === functionType ? helper.call(depth0, {"name":"title","hash":{},"data":data}) : helper));
   if(stack1 || stack1 === 0) { buffer += stack1; }
   buffer += "</div>\n		<div class=\"blurb\">"
