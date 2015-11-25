e6f46b18387ccaa547fd804f10b8bf4e5df77db9
Ann Yuan <ayuan@thisisgrow.com>
2015-11-25 10:48:10 -0500

    link videos to project pages

--- js/projects/wwit.js
+++ js/projects/wwit.js
@@ -6,7 +6,7 @@ define(['templates/project_detail', 'project_data'], function(projectTemplate, p
         identifier: "wwit",
         title: "<a href='http://whatwouldittake.cc' target='_blank'>wwit</a>",
         blurb: "What would it take to change your mind?",
-        projectContents: '<div class="chrome"><div class="chrome-controls"><div class="close"></div><div class="min"></div><div class="max"></div></div><div class="contents"><video src="/video/wwit.mp4"></video></div></div>',
+        projectContents: '<a href="http://whatwouldittake.cc" target="_blank" class="chrome"><div class="chrome-controls"><div class="close"></div><div class="min"></div><div class="max"></div></div><div class="contents"><video src="/video/wwit.mp4"></video></div></a>',
         caption: "<div class='loading'>Loading video...</div><div class='caption-0'>State your belief.</div><div class='caption-1'>Ask yourself what it would take to change your mind.</div><div class='caption-2'>Consider opposing evidence.</div><div class='caption-3'>Change your belief, and see how public opinion has evolved.</div><div class='caption-4'>Change your belief, and see how public opinion has evolved.</div>",
         description: "<a class='cta-main' href='http://whatwouldittake.cc' target='_blank'>View project</a><p>Project overview contents.</p><br/><div class='section-header'>What's the point?</div><p>The point.</p><br/><div class='section-header'>Technology</div><p>WWIT is built with <a target='_blank' href='https://github.com/Matt-Esch/virtual-dom'>virtual dom</a>, <a target='_blank' href='http://d3js.org/'>d3</a>, <a target='_blank' href='https://nodejs.org/en/'>Node</a>, and <a target='_blank' href='https://mongodb.com'>MongoDB</a>.</p>"
       };
