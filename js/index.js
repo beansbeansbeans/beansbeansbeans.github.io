2315b2b4234b780da6f3c9495efb996e7373546a
Ann Yuan <ayuan@thisisgrow.com>
2016-01-20 20:51:12 -0500

    remove test

--- js/index.js
+++ js/index.js
@@ -27,18 +27,6 @@ define("d3.global", ["d3"], function(_) {
 	d3 = _;
 });
 
-var request = new XMLHttpRequest();
-request.open('GET', '//oughtness-49671.onmodulus.net/causes', true);
-
-request.setRequestHeader("Accept", "application/json");
-
-request.onload = function() {
-  console.log("hi");
-  console.log(request.response);
-};
-
-request.send();
-
 require(['jquery', 'router', 'loader'], function($, Router, Loader) {
 
     var router = new Router(),
